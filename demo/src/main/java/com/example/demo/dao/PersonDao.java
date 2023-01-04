96062a2fe1fe6f3103e7a5729b0006d9dc692647
Bill-W315 <50357170+Bill-W315@users.noreply.github.com>
2023-01-04 18:24:43 +0800

    project init

---
+++ demo/src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,15 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    int insertPerson(Person person);
+
+    List<Person> getPerson();
+
+}
---
+++ demo/src/main/java/com/example/demo/dao/PersonImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.dao;
+
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class PersonImpl implements PersonDao{
+
+    private static List<Person> personList = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person){
+        personList.add(new Person(id,person.getName()));
+        return 1;
+    }
+
+    @Override
+    public int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        personList.add(new Person(id,person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> getPerson(){
+        return personList;
+    }
+
+}
